82f86a31cb6f935beb5832c93e388a97fb8ee56c
Michael Kuklik <mkuklik@gmail.com>
2021-03-30 12:28:02 -0700

    removed tcp version

--- raftTCP/protocol.go
+++
@@ -1,6 +0,0 @@
-package rafttcp
-
-// https://ieftimov.com/post/understanding-bytes-golang-build-tcp-protocol/
-// https://blog.golang.org/gob
-
-// when joining follower, follower will reply with message RedirectMessage, which
